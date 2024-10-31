cfd261f6aac33b74b50e3c3e90e913353e639add
gummi <gummi@github.com>
2024-10-30 23:28:01 -0500

    bash abash

--- cli.sh
+++ cli.sh
@@ -1,45 +1,49 @@
 #!/bin/bash
 
 # Function to display the menu
-function show_menu {
-    echo ""
-    echo "Commands:"
-    echo "1. new <data>"
-    echo "2. get <number>"
-    echo "3. add <data>"
-    echo "4. delete <number>"
-    echo "5. clear"
-    echo "6. exit"
-}
+echo ""
+echo "Commands:"
+echo "  new"
+echo "  get <number>"
+echo "  add"
+echo "  delete <number>"
+echo "  clear"
+echo "  exit"
+
 
 # Ask for the web server link
-read -p "Enter the web server link: " server_link
+read -p "Enter the pastebin link: " server_link
 
 # Main loop
 while true; do
-    show_menu
-    read -p "Enter command: " command arg1
+    read -p "> " command arg1
 
     case $command in
         new)
             read -p "Enter data for new: " data
             curl -X POST -d "$data" "$server_link/new"
+	    echo ""
             ;;
         get)
             curl "$server_link/raw/$arg1"
+	    echo ""
             ;;
         add)
             read -p "Enter data to add: " data
             curl -X POST -d "$data" "$server_link"
+	    echo ""
             ;;
         delete)
             curl -X POST "$server_link/$arg1/delete"
+	    echo ""
             ;;
         clear)
             curl -X POST "$server_link/clear"
+	    echo ""
             ;;
         exit)
             echo "Exiting..."
+	    echo ""
             break
             ;;
         *)
